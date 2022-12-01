c0cb25d7ff8f5a6efb8aa41dea3f17841b48ec9b
Tino Heuberger <tino.heuberger@fhnw.ch>
2022-12-01 13:49:28 +0100

    Setup repo

---
+++ 01/sol.cs
@@ -0,0 +1,11 @@
+public class dec01 {
+    public void exec() {
+        var lines = File.ReadAllLines("./01/input");
+        
+        var elfs_food = new Dictionary<int, int[]>();
+
+        foreach (var line in lines) {
+
+        }
+    }
+}
